31545a39ffbec74b4d57c883afe9cfc42cb7c3c8
samfromaway@gmail.com <email@example.com>
2020-05-02 17:04:05 -0500

    javascript delete contact form border red

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -11,30 +11,6 @@ const navSlide = () => {
 
 navSlide();
 
-// Contact Form Red Border
-
-$(function () {
-  $('#myform').submit(function () {
-    var valid = 0;
-    $(this)
-      .find('input[type=text], textarea[name=message]')
-      .each(function () {
-        if ($(this).val() != '') {
-          valid++;
-          $(this).css('border-color', 'initial');
-        } else {
-          $(this).css('border-color', 'red');
-        }
-      });
-
-    if (valid > 0) {
-      return true;
-    } else {
-      return false;
-    }
-  });
-});
-
 //---- Observer ----//
 const appearOptions = {
   threshold: 0,

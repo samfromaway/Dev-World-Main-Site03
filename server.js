e5607112ea24d85ebfe2eeabfa3fea26a49275e6
samfromaway <samfromaway@gmail.com>
2023-12-31 15:19:35 +0100

    fix

---
+++ server.js
@@ -0,0 +1,12 @@
+// server.js
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.redirect("https://www.samfromaway.com");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
